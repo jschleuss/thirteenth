c7b5809cd8e6485b156f7261d911c437697505ee
Jon Schleuss <jschleuss@me.com>
2013-04-05 08:37:55 -0500

    Initial commit

--- thirteenth.py
+++ thirteenth.py
@@ -10,4 +10,4 @@ for year in range(current_year, current_year+101):
 	for day in range(1,13):
 		if datetime.datetime(year,day,13).weekday() == 4:
 			# prints dates with Friday the 13th
-			print datetime.datetime(year,day,13).strftime("%m/%d/%y")
+			print datetime.datetime(year,day,13).strftime("%m/%d/%y") 
